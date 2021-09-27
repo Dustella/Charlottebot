76ffe5b78b1a69b9120627cfe578738097488317
Dustella <fdnoaivj@outlook.com>
2021-09-27 11:45:37 +0800

    改进了自动机

--- src/plugins/auto/__init__.py
+++ src/plugins/auto/__init__.py
@@ -3,7 +3,7 @@ from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Bot,Event,Message,MessageEvent,GroupMessageEvent,PrivateMessageEvent
 import json
 
-auto=on_startswith('.')
+auto=on_startswith({'.','。','~'})
 
 @auto.handle()
 async def autoi(bot:Bot,event:GroupMessageEvent):
@@ -12,8 +12,20 @@ async def autoi(bot:Bot,event:GroupMessageEvent):
     print(args)
     if '[CQ:at' in args:
         print('OK')
-        text=json.loads(event.json())['message'][0]['data']['text'].replace('.','')
-        source_qq=event.sender.user_id
-        target_qq=json.loads(event.json())['message'][1]['data']['qq']
-        target_msg=f"[CQ:at,qq={source_qq}] {text}了[CQ:at,qq={target_qq}]！~"
-        await auto.finish(Message(target_msg))
+        mjs=json.loads(event.json())['message']
+        print(mjs)
+        if len(mjs)==2:
+            text=json.loads(event.json())['message'][0]['data']['text'].replace('.','')
+            source_qq=event.sender.user_id
+            target_qq=json.loads(event.json())['message'][1]['data']['qq']
+            target_msg=f"[CQ:at,qq={source_qq}] {text}了[CQ:at,qq={target_qq}]！~"
+            await auto.finish(Message(target_msg))
+        elif len(mjs)==3:
+            text1=json.loads(event.json())['message'][0]['data']['text'].replace('.','')
+            text2=json.loads(event.json())['message'][2]['data']['text']
+            source_qq=event.sender.user_id
+            target_qq=json.loads(event.json())['message'][1]['data']['qq']
+            target_msg=f"[CQ:at,qq={source_qq}] {text1} [CQ:at,qq={target_qq}] {text2}！~"
+            await auto.finish(Message(target_msg))
+        else:
+            await auto.finish("啦啦啦？")
