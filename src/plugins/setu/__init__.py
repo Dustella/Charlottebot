75019e35102de4c9ac207ea873355f2e2302aea1
Dustella <fdnoaivj@outlook.com>
2021-09-27 11:04:25 +0800

    修改图源

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -1,16 +1,17 @@
 from nonebot import on_keyword
 from nonebot.adapters.cqhttp import Bot,Event,Message
 from nonebot.typing import T_State
+from random import randint
 import httpx
 
 
-setu = on_keyword({"涩图来","色图来"})
+setu = on_keyword({"猫猫来"})
 
 @setu.handle()
 async def handle(bot: Bot, event: Event, state: T_State):
     async with httpx.AsyncClient() as client:
-        resp = await client.get('https://api.nyan.xyz/httpapi/sexphoto')
-        imgurl = resp.json()['data']['url'][0]
+        resp = await client.get('https://someacg.rocks/api/list?page=2').json()
+        imgurl = resp["list"][randint(1,30)]["picUrl"]
         cqimg = f"[CQ:image,file=1.{imgurl.split('.')[1]},url={imgurl}]"
         await setu.send(Message(cqimg))
 
