83f86f04c543483eac48f72cc41c9a25278a2b1c
Dustella <fdnoaivj@outlook.com>
2021-09-27 11:29:26 +0800

    哦，我这个大笨蛋，居然尝试json化协程对象，我的妈妈会打我的屁股的

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -10,8 +10,9 @@ setu = on_keyword({"猫猫来"})
 @setu.handle()
 async def handle(bot: Bot, event: Event, state: T_State):
     async with httpx.AsyncClient() as client:
-        resp = await client.get('https://someacg.rocks/api/list?page=2').json()
-        imgurl = resp["list"][randint(1,30)]["picUrl"]
+        resp = await client.get('https://someacg.rocks/api/list?page=2')
+        imgurl = resp.json()["list"][randint(1,30)]["picUrl"]
+        
         cqimg = f"[CQ:image,file=1.{imgurl.split('.')[1]},url={imgurl}]"
         await setu.send(Message(cqimg))
 
