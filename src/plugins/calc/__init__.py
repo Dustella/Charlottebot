841c5bcc45f82a2a847d45df1a3e4c3b60afeffd
Dustella <fdnoaivj@outlook.com>
2021-10-09 11:14:44 +0800

    calc sec promt

--- src/plugins/calc/__init__.py
+++ src/plugins/calc/__init__.py
@@ -7,12 +7,11 @@ calc=on_command('calc')
 @calc.handle()
 async def claaa(bot:Bot,event:MessageEvent):
     ban_list={"os","import","open","delete","__"}
-    if str(event.get_message()) in ban_list: calc.finish()
+    if str(event.get_message()) in ban_list: calc.finish("dangourous expression")
     args=str(event.get_message()).strip()
     try:
         result=eval(args)
     except:
         result='Bad Expression'
-    print(str(result))
     result=str(result)
     await calc.finish(result)
