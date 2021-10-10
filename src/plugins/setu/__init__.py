6c8816f2bb8754a54919fe6a924fffe30ff4cad7
Dustella <fdnoaivj@outlook.com>
2021-10-10 23:14:23 +0800

    prettier

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -12,6 +12,8 @@ async def handle(bot: Bot, event: Event, state: T_State):
     async with httpx.AsyncClient() as client:
         resp = await client.get('https://someacg.rocks/api/random?detail=1')
         imgurl = resp.json()["picUrl"]
+        # get image Here
+
         cqimg = f"[CQ:image,file=1.{imgurl.split('.')[1]},url={imgurl}]"
         await setu.send(Message(cqimg))
 
@@ -22,5 +24,7 @@ async def cat(bot:Bot,event:Event):
     async with httpx.AsyncClient() as client:
         resp = await client.get('https://api.thecatapi.com/v1/images/search')
         imgurl = resp.json()[0]["url"]
+        # get image Here
+
         cqimg = f"[CQ:image,file=1.{imgurl.split('.')[1]},url={imgurl}]"
         await setu.send(Message(cqimg))
