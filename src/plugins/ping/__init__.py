679ced58c146fabb14fc02d72e9bdead38fb1d5b
Dustella <fdnoaivj@outlook.com>
2021-10-16 12:48:02 +0800

    ping added

---
+++ src/plugins/ping/__init__.py
@@ -0,0 +1,31 @@
+from nonebot import get_driver, on_command
+from nonebot.adapters.cqhttp import Bot, MessageEvent
+from .config import Config
+from requests import get
+import json
+
+global_config = get_driver().config
+config = Config(**global_config.dict())
+
+ping = on_command('ping')
+
+
+@ping.handle()
+async def ping_ip(bot: Bot, event: MessageEvent):
+    req_ping = {"host": str(event.message)}
+    req_icp={"domain":str(event.message)}
+    try:
+        response_ping = get(url="https://api.ooii.io/ping", params=req_ping).json()
+        if response_ping['state'] == 1001:
+            await ping.finish("域名错误")
+        response_icp=get(url="https://api.ooii.io/beian/api.php",params=req_icp).json()
+        tosend = f"""
+域名：{response_ping["host"]}
+ip: {response_ping["ip"]}
+地点: {response_ping["location"]}
+备案单位：{response_icp["unitName"]}
+ICP：{response_icp["icp"]}
+        """
+        await ping.finish(tosend)
+    except Exception as e:
+        await ping.finish(str(e))
