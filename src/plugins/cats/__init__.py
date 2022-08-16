09a368efbf46cca447e94cacd0001d3d8342e60c
Dustella Chan <fdnoaivj@outlook.com>
2022-08-16 15:10:30 +0800

    feat: 可以请求猫娘了

--- src/plugins/cats/__init__.py
+++ src/plugins/cats/__init__.py
@@ -13,3 +13,13 @@ async def cat(bot: Bot, event: Event):
 
         cqimg = f"[CQ:image,file=1.{imgurl.split('.')[1]},url={imgurl}]"
         await cats.send(Message(cqimg))
+
+cat_girl = on_keyword({"猫娘来","来点猫娘"})
+
+@cat_girl.handle()
+async def cat_g(bot:Bot,event:Event):
+    async with httpx.AsyncClient() as client:
+        resp = await client.get('https://api.r10086.com:8443/img-api.php?type=猫娘1')
+        location = f"https://api.r10086.com:8443{resp.headers['location']}"
+        image = f"[CQ:image,file=1.{location.split('.')[1]},url={location}]"
+        await cat_girl.finish(Message(image))
