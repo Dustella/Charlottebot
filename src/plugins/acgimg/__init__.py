d1e4dbca29366a1054b3a59519e0afd8e8f0d07b
Dustella <fdnoaivj@outlook.com>
2023-01-01 15:40:47 +0800

    🎨 修复ACG和猫娘

--- src/plugins/acgimg/__init__.py
+++ src/plugins/acgimg/__init__.py
@@ -1,18 +1,16 @@
 from nonebot import on_command, on_keyword
 from nonebot.adapters.onebot.v11 import Bot, Event, Message
-import httpx
+from requests import get
 from random import randint
 
 acgimg = on_keyword({"来点色色", "来点涩涩", "我想色色", "我想涩涩",
                     "色图来", "瑟图来", "涩图来", "来点瑟瑟"})
 acgimg_command = on_command("acgimage")
 
+
 @acgimg_command.handle()
 @acgimg.handle()
 async def cat(bot: Bot, event: Event):
-    async with httpx.AsyncClient() as client:
-        list = (await client.get(f'https://someacg.rocks/api/list?page={randint(1, 25)}')).json()["body"]
-        imgurl = "https://cdn.someacg.rocks/images/" + \
-            list[(randint(0, 29))]["file_name"]
-        cqimg = f"[CQ:image,file=1.{imgurl.split('.')[1]},url={imgurl}]"
-        await acgimg.send(Message(cqimg))
+    url = get('https://acg-img.dustella.net/api').json()["url"]
+    cqimg = f"[CQ:image,file=1.{url.split('.')[1]},url={url}]"
+    await acgimg.send(Message(cqimg))
--- src/plugins/cats/__init__.py
+++ src/plugins/cats/__init__.py
@@ -1,6 +1,7 @@
 from nonebot import on_keyword
 from nonebot.adapters.onebot.v11 import Bot, Event, Message
 import httpx
+from requests import get
 
 cats = on_keyword({"猫猫来", "猫来", "猫再来"})
 
@@ -14,12 +15,13 @@ async def cat(bot: Bot, event: Event):
         cqimg = f"[CQ:image,file=1.{imgurl.split('.')[1]},url={imgurl}]"
         await cats.send(Message(cqimg))
 
-cat_girl = on_keyword({"猫娘来","来点猫娘"})
+cat_girl = on_keyword({"猫娘来", "来点猫娘"})
+
 
 @cat_girl.handle()
-async def cat_g(bot:Bot,event:Event):
-    async with httpx.AsyncClient() as client:
-        resp = await client.get('https://api.r10086.com:8443/img-api.php?type=猫娘1')
-        location = f"https://api.r10086.com:8443{resp.headers['location']}"
-        image = f"[CQ:image,file=1.{location.split('.')[1]},url={location}]"
-        await cat_girl.finish(Message(image))
+async def cat_g(bot: Bot, event: Event):
+    resp = get('https://acg-img.dustella.net/cats')
+    location = resp.json()["url"]
+    print(location)
+    image = f"[CQ:image,file=1.{location.split('.')[1]},url={location}]"
+    await cat_girl.finish(Message(image))
